d1500fc5cffd0f580411198ecaba72fdae3dddae
IuryPrates <prates700@gmail.com>
2022-12-07 12:39:00 -0400

    feat: add flip button and configure default camera

--- oneBithCam/App.js
+++ oneBithCam/App.js
@@ -1,20 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  StyleSheet,
+  Text, 
+  View,
+  SafeAreaView,
+  TouchableOpacity
+} from 'react-native';
+import styles from './style';
+import { Camera } from 'expo-camera';
+import { useEffect, useState } from 'react'
+import { FontAwesome } from "@expo/vector-icons"
 
 export default function App() {
+
+  const [type, setType] = useState(Camera.Constants.Type.back)
+  const [hasPermission, setHasPermission] = useState(null)
+
+  //permissiom for use camera
+  useEffect(() => {
+    (async () => {
+      const {status} = await Camera.requestCameraPermissionsAsync()
+      setHasPermission(status === 'granted');
+    })();
+  }, [])
+
+  if(hasPermission === null){
+    return <View/>
+  }
+  if(hasPermission === false){
+    return <Text>Acesso negado</Text>
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.initView}>
+      <Camera
+      style={styles.camera}
+      type={type}
+      >
+        <View style={styles.buttonCam}>
+          <TouchableOpacity
+          style={styles.buttonFlip}
+          onPress={() =>
+            setType(
+              type === Camera.Constants.Type.back 
+              ? Camera.Constants.Type.front 
+              : Camera.Constants.Type.back
+            )}
+          >
+            <FontAwesome name="exchange" size={23} color={"red"}></FontAwesome>
+          </TouchableOpacity>
+        </View>
+      </Camera>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ oneBithCam/style.js
@@ -0,0 +1,33 @@
+import { StyleSheet } from "react-native"
+
+const styles = StyleSheet.create({
+    initView: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    camera: {
+        width: "100%",
+        height: "100%",
+    },
+    buttonCam: {
+        flex: 1,
+        backgroundColor: "transparent",
+        flexDirection: "row",
+    },
+    buttonFlip: {
+        position: "absolute",
+        bottom : 50,
+        left: 30,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#fff",
+        margin: 20,
+        height: 50,
+        width: 50,
+        borderRadius: 50,
+    }
+})
+
+export default styles;
